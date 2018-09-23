e2c7cbdcb68c78cea8b5b33376610f5aff3c2c24
lazycat <lazycat_develop@163.com>
2018-09-23 19:27:12 +0800

    add maze project

--- reinforcement_learning/taxiV2/agent.py
+++ reinforcement_learning/taxiV2/agent.py
@@ -13,7 +13,7 @@ class Agent:
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
 
-    def select_action(self, state):
+    def select_action(self, state, i_episode):
         """ Given the state, select an action.
 
         Params
@@ -24,9 +24,9 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
+        return np.random.choice(self.nA, p=self.get_action_prob(i_episode, state))
 
-    def step(self, state, action, reward, next_state, done):
+    def step(self, state, action, reward, next_state, done, i_episode, alpha=1, gamma=1.0):
         """ Update the agent's knowledge, using the most recently sampled tuple.
 
         Params
@@ -37,4 +37,16 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+
+        self.Q[state][action] = Agent.update_Q(self.Q[state][action], np.dot(self.Q[next_state],\
+                                self.get_action_prob(i_episode, state)), reward, alpha, gamma)
+
+    @staticmethod
+    def update_Q(Qsa, Qsa_next, reward, alpha, gamma):
+        return Qsa + (alpha * (reward + (gamma * Qsa_next) - Qsa))
+
+    def get_action_prob(self, i_episode, state):
+        epsilon = 1.0 / i_episode
+        action_prob = np.ones(self.nA) * epsilon / self.nA
+        action_prob[np.argmax(self.Q[state])] = 1 - epsilon + (epsilon / self.nA)
+        return action_prob

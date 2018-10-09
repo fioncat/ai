e315863d5960686c7bfd1b1faec9a961b6558166
lazycat <lazycat_develop@163.com>
2018-10-09 18:21:11 +0800

    rename

--- deeplearning/cnn/tf-vgg/train.py
+++ deeplearning/cnn/tf-vgg/train.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python
+import os
+
+import numpy as np
+import tensorflow as tf
+
+from tensorflow_vgg import vgg16
+from tensorflow_vgg import utils
+
+data_dir = 'data/flower_photos'
+
+contents = os.listdir(data_dir)
+classes = [each for each in contents if os.path.isdir(data_dir + each)]
+
+print("all classes", classes)
